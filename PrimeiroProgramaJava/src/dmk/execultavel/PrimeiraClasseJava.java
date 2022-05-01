184f55808bb1e12260a0e1a188055e12b68f308a
DigitalMK <marcio@digitalmk.com.br>
2022-05-01 16:04:18 -0300

    Inicio dos estudos jrojeto 1

---
+++ PrimeiroProgramaJava/src/dmk/execultavel/PrimeiraClasseJava.java
@@ -0,0 +1,11 @@
+package dmk.execultavel;
+
+public class PrimeiraClasseJava 
+{ 
+	public static void main(String[] args) {
+		
+		System.out.println("Bem vindo a programação java ");
+		System.out.println("Bem vindo a programação java ");
+	}
+
+}
