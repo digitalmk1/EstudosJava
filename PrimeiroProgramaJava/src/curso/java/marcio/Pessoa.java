6079639336bd6c744f088847812404709a2861d9
DigitalMK <marcio@digitalmk.com.br>
2022-05-01 19:40:05 -0300

    Criação do field data;

--- PrimeiroProgramaJava/src/curso/java/marcio/Pessoa.java
+++ PrimeiroProgramaJava/src/curso/java/marcio/Pessoa.java
@@ -5,31 +5,41 @@ public class Pessoa {
 	private String nome;
 	private String sobrenome;
 	private String email;
-	
-	public String getEmail()
-	{
+	private String data;
+
+	public String getData() {
+
+		return data;
+	}
+
+	public void setData() {
+
+		this.data = data;
+	}
+
+	public String getEmail() {
 		return email;
 	}
-	
-	public void setEmail(String email)
-	{
-		
+
+	public void setEmail(String email) {
+
 		this.email = email;
 	}
-		
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getSobrenome() {
 		return sobrenome;
 	}
+
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
 	}
-	
-	
-	
+
 }
