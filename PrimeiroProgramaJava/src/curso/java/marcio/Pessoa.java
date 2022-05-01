62f8ebb3cd25eb4b1b955ab87096697aaded6fe8
DigitalMK <marcio@digitalmk.com.br>
2022-05-01 19:46:50 -0300

    Revert "Criação do field data;"
    This reverts commit 6079639336bd6c744f088847812404709a2861d9.

--- PrimeiroProgramaJava/src/curso/java/marcio/Pessoa.java
+++ PrimeiroProgramaJava/src/curso/java/marcio/Pessoa.java
@@ -5,41 +5,31 @@ public class Pessoa {
 	private String nome;
 	private String sobrenome;
 	private String email;
-	private String data;
-
-	public String getData() {
-
-		return data;
-	}
-
-	public void setData() {
-
-		this.data = data;
-	}
-
-	public String getEmail() {
+	
+	public String getEmail()
+	{
 		return email;
 	}
-
-	public void setEmail(String email) {
-
+	
+	public void setEmail(String email)
+	{
+		
 		this.email = email;
 	}
-
+		
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 	public String getSobrenome() {
 		return sobrenome;
 	}
-
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
 	}
-
+	
+	
+	
 }
