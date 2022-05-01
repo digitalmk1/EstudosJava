8ae5e7d0e7b426e72a5ddcbbfc2cc2ba61fb76ff
DigitalMK <marcio@digitalmk.com.br>
2022-05-01 16:32:38 -0300

    colocando a senha do quithub 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

--- PrimeiroProgramaJava/src/dmk/execultavel/PrimeiraClasseJava.java
+++ PrimeiroProgramaJava/src/dmk/execultavel/PrimeiraClasseJava.java
@@ -5,7 +5,8 @@ public class PrimeiraClasseJava
 	public static void main(String[] args) {
 		
 		System.out.println("Bem vindo a programação java ");
-		System.out.println("Bem vindo a programação java ");
+		System.out.println(" senha do quit hub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");
+	
 	}
 
 }
