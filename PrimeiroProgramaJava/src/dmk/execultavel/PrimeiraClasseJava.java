67c7aaf7639423b3cdb6e3319416891773292684
DigitalMK <marcio@digitalmk.com.br>
2022-05-01 19:17:49 -0300

    teste com nova senha

--- PrimeiroProgramaJava/src/dmk/execultavel/PrimeiraClasseJava.java
+++ PrimeiroProgramaJava/src/dmk/execultavel/PrimeiraClasseJava.java
@@ -1,12 +1,18 @@
 package dmk.execultavel;
 
+import curso.java.marcio.Pessoa;
+
 public class PrimeiraClasseJava 
 { 
 	public static void main(String[] args) {
 		
-		System.out.println("Bem vindo a programação java ");
-		System.out.println(" senha do quit hub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");
+	Pessoa pessoa = new Pessoa();
+	pessoa.setEmail("pessoa@gmail.com");
+	pessoa.setNome("Marcio");
 	
 	}
+	
+	
+  
 
 }
