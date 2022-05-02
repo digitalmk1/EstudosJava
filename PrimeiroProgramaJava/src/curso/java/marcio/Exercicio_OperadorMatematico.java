878b2f83a1c4c113f511521c42b5ed332b7f3586
DigitalMK <marcio@digitalmk.com.br>
2022-05-02 10:53:54 -0300

    operador %

---
+++ PrimeiroProgramaJava/src/curso/java/marcio/Exercicio_OperadorMatematico.java
@@ -0,0 +1,16 @@
+package curso.java.marcio;
+
+public class Exercicio_OperadorMatematico {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+     double carro = 9;
+     double pessoa = 2;
+     
+     double resto = carro % pessoa;
+     
+     System.out.println("Sobrarão exatamente : " + resto + " carros ");
+	}
+
+}
