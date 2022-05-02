716567bf44f5d695daf88e5b49726cc906e1e87f
DigitalMK <marcio@digitalmk.com.br>
2022-05-01 22:05:24 -0300

    Exercicio Switch Case

---
+++ PrimeiroProgramaJava/src/curso/java/marcio/Exercicio_Switch_Case.java
@@ -0,0 +1,27 @@
+package curso.java.marcio;
+
+public class Exercicio_Switch_Case {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+	
+		int dia = 8;
+		switch (dia) {
+		case 1:
+			System.out.println("pagar contas");
+			
+			break;
+		case 2:
+			System.out.println("viajar para Salvador");
+			break;
+		case 3:
+			System.out.println("Trabalhar fora");
+				break;	
+          
+		default: System.out.println("outro dia");
+			break;
+		}
+	}
+
+}
