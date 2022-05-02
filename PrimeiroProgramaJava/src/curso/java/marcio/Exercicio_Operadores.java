cb23920a7e896b7c5246dbf69aa4b5bdc70beb0f
DigitalMK <marcio@digitalmk.com.br>
2022-05-01 22:15:45 -0300

    Operadores

---
+++ PrimeiroProgramaJava/src/curso/java/marcio/Exercicio_Operadores.java
@@ -0,0 +1,24 @@
+package curso.java.marcio;
+
+public class Exercicio_Operadores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int nota1 = 90;
+		int nota2 = 90;
+		
+		if( nota1 == nota2)
+		{
+			
+			System.out.println("SAO DIFERENTES");
+		}
+		else
+		{
+			
+			System.out.println("SAO IGUAIS");
+		}
+
+	}
+
+}
