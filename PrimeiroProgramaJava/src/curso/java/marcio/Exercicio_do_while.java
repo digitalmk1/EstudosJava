629be4746d383b664f75fa49057b849099f056df
DigitalMK <marcio@digitalmk.com.br>
2022-05-01 23:05:38 -0300

    do While

---
+++ PrimeiroProgramaJava/src/curso/java/marcio/Exercicio_do_while.java
@@ -0,0 +1,24 @@
+package curso.java.marcio;
+
+public class Exercicio_do_while {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int numero = 0;
+		
+		while(numero < 100) {
+			numero ++;
+			System.out.println("O numero atual é: " + numero);
+			
+			
+		}
+		
+		/*------------------------------------------------*/
+		int numero2 = 0;
+		do {
+			numero2 ++;
+			System.out.println("O numero atual é: " + numero2);
+		} while (numero2 <=60);
+	}
+}
