9b4daa1e47baece2dcc0dc79b287ffe101df1152
DigitalMK <marcio@digitalmk.com.br>
2022-05-01 23:06:11 -0300

    For com condicionais

---
+++ PrimeiroProgramaJava/src/curso/java/marcio/Exercicio_for.java
@@ -0,0 +1,16 @@
+package curso.java.marcio;
+
+public class Exercicio_for {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		
+		for(int numero = 0; numero <=10; numero++) {
+			if(numero == 3 || numero == 6 || numero == 9 ) {
+				System.out.println("Oba encontrei o numero "+ numero);
+				continue;
+			}
+		}
+	}
+}
